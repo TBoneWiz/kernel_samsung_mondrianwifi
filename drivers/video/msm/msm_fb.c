11753dbf6ca37a5092fdb1d17604e2b73c10dab6
TboneWiz <netfluke@yahoo.com>
2016-11-13 10:57:58 -0500

    msm: mdp: wait for previous frame commit before unset
    This change resolves the flicker on the display panel which is caused because there is no wait for previous commit to finish before unsetting the mdp pipe configuration.
    
    Change-Id: I817c5273def19223d5648778dfc3d2138317aad8
    Signed-off-by: Rahul Sharma <sharah@codeaurora.org>
    (cherry picked from commit f8cefaeef2a46a9e38e7664d13d7ee80b743dc66)
    
    https://review.cyanogenmod.org/#/c/170264/

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -3025,12 +3025,26 @@ static int msmfb_overlay_unset(struct fb_info *info, unsigned long *argp)
 {
 	int ret, ndx;
 
+	struct msm_fb_data_type *mfd;
+
+	if (info == NULL || info->par == NULL) {
+		pr_err("%s info=%p or par is NULL\n", __func__, info);
+		return -ENODEV;
+	}
+
+	mfd = (struct msm_fb_data_type *)info->par;
 	ret = copy_from_user(&ndx, argp, sizeof(ndx));
 	if (ret) {
 		printk(KERN_ERR "%s:msmfb_overlay_unset ioctl failed \n",
 			__func__);
 		return ret;
 	}
+
+	/*
+	 * If previous commit hasn't finished yet, unset cannot be started
+	 * otherwise, previous scene will be corrupted.
+	 */
+	msm_fb_pan_idle(mfd);
 
 	return mdp4_overlay_unset(info, ndx);
 }
