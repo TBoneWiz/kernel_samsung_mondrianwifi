92942e5fdea2170cbc2a584c35ef75c395528bc4
TboneWiz <netfluke@yahoo.com>
2017-05-29 10:30:47 -0400

    msm: camera: restructure data handling to be more robust
    Add dynamic array allocation instead of static to prevent
    stack overflow.
    
    Change-Id: I0c1ce933ffaa43d073d42558b8292f832f22c720
    CRs-Fixed: 789704
    Signed-off-by: Vasko Kalanoski <vaskok@codeaurora.org>
    Signed-off-by: VijayaKumar T M <vtmuni@codeaurora.org>
    
    Mirror commit bd4990ee47bd48e6e87b0106d7d14090e90eb0c5 to tabpro directory

--- drivers/media/platform/msm/camera_v2_tabpro/sensor/io/msm_camera_cci_i2c.c
+++ drivers/media/platform/msm/camera_v2_tabpro/sensor/io/msm_camera_cci_i2c.c
@@ -175,6 +175,12 @@ int32_t msm_camera_cci_i2c_write_seq(struct msm_camera_i2c_client *client,
 		|| num_byte == 0)
 		return rc;
 
+	if (num_byte > I2C_SEQ_REG_DATA_MAX) {
+		pr_err("%s: num_byte=%d clamped to max supported %d\n",
+			__func__, num_byte, I2C_SEQ_REG_DATA_MAX);
+		return rc;
+	}
+
 	S_I2C_DBG("%s reg addr = 0x%x num bytes: %d\n",
 			  __func__, addr, num_byte);
 
